2b07eaca9d5f7f052a6293396bb85205c6ce5190
liumeng <1808728912@qq.com>
2021-12-22 18:13:02 +0800

    播放器滚动同步歌词开发

--- src/pages/Player/components/Play/index.tsx
+++ src/pages/Player/components/Play/index.tsx
@@ -1,6 +1,6 @@
 import { View,Image } from "@tarojs/components";
 import Taro from '@tarojs/taro'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import {useDispatch,useSelector} from 'react-redux'
 
 import play1Img from '@/img/play1.png'
@@ -12,6 +12,9 @@ import nextImg from '@/img/next.png'
 import proImg from '@/img/pro.png'
 import playImg from '@/img/bofang-4.png'
 import ListImg from '@/img/list.png'
+import playingImg from '@/img/palying.png'
+import suijiImg from '@/img/suiji.png'
+import xunhuan1Img from '@/img/xunhuan1.png'
 
 import './index.scss'
 import { SONG_LIST_INDEX } from '../../../../stores/constants/index';
@@ -20,28 +23,40 @@ import { SONG_LIST_INDEX } from '../../../../stores/constants/index';
 export default function Play(props) {
   const dispatch = useDispatch();
   const {songListIndex}  = useSelector((state: any) => state.player)
+  const [playingFlag, setPalyingFlag] = useState(false) //播放状态
   const [songList, setSongList] = useState(props.demoSongList)
+  var [songOrder, setSongOrder] = useState(1)
   const [innerAudioContext, setInnerAudioContext] = useState(Taro.createInnerAudioContext())
   var [songIndex,setsongIndex] = useState(songListIndex ? songListIndex : 0)
   const [duration, setDuration] = useState(0)
   const [currentTime, setCurrentTime] = useState(innerAudioContext.currentTime)
 
-
+  // 监听播放时间 
+  useEffect(() => {
+    props.changeCurrentTime(currentTime)
+    // console.log(currentTime)
+  },[currentTime])
 
   // 播放/暂停
   const handlePlaySong = async() => {
     // 暂停/播放音乐
     if(!innerAudioContext.src){
-      innerAudioContext.src = songList[songIndex].url
+      // innerAudioContext.src = songList[songIndex].url
+      innerAudioContext.src = 'http://localhost:8091/mp3/1903299149.mp3'
     }
     if(innerAudioContext.paused ){
       await innerAudioContext.play()
+      setPalyingFlag(true)
       setDuration(innerAudioContext.duration)
-      setInterval((params) => {
+      setInterval(() => {
         setCurrentTime(innerAudioContext.currentTime)
       },100)
+      // innerAudioContext.onTimeUpdate()
+      props.handlSong(innerAudioContext,true)
     }else{
       innerAudioContext.pause()
+      setPalyingFlag(false)
+      props.handlSong(innerAudioContext,false)
     }
   }
   // 上一曲
@@ -56,7 +71,6 @@ export default function Play(props) {
       payload: songIndex,
     })
     innerAudioContext.src = songList[songIndex].url
-    handlePlaySong()
   }
   // 下一曲
   const handleNextSong = () => {
@@ -80,9 +94,24 @@ export default function Play(props) {
     console.log('跳转到指定位置')
     innerAudioContext.seek(1.35) // 单位 s
   }
-  const stom = (data) => {
-    return  (data / 60).toFixed(2)
+
+  // 时间转换
+  const currentTimeformat  = (value) =>{
+    if (!value) return ''
+    const interval = Math.floor(value)
+    const minute = (Math.floor(interval / 60)).toString().padStart(2, '0')
+    const second = (interval % 60).toString().padStart(2, '0')
+    return `${minute}:${second}`
   }
+  // 修改播放顺序
+  const handleChangeSongOrder = () => {
+    if(songOrder ===3 ){
+      setSongOrder(1)
+    }else {
+      setSongOrder(++songOrder)
+    }
+  }
+
   return (
     <View className="player__bottom">
       <View className="player__bottom__top">
@@ -97,22 +126,23 @@ export default function Play(props) {
       </View>
       <View className="player__bottom__conter">
         {/* 播放时间的移动 */}
-        <View className="player__bottom__conter__start_time">{stom(currentTime) }</View>
+        <View className="player__bottom__conter__start_time">{currentTime ? currentTimeformat(currentTime)  : '00:00'}</View>
         <View className="player__bottom__conter__progress"></View>
-        <View className="player__bottom__conter__end_time">{innerAudioContext.duration? stom(innerAudioContext.duration) :'00:00'}</View>
+        <View className="player__bottom__conter__end_time">{innerAudioContext.duration? currentTimeformat(innerAudioContext.duration) :'00:00'}</View>
       </View>
       <View className="player__bottom__bottom">
         {/* 顺序切换 */}
         <Image
-          src={xunhuanImg}
+          src={songOrder === 1 ?  xunhuanImg   : (songOrder === 2 ?  suijiImg  : xunhuan1Img )}
+          onClick={handleChangeSongOrder}
           className="player__bottom__bottom__play-order"
         />
         <Image src={proImg} className="player__bottom__bottom__last-song" onClick={handleProSong} />
-        <Image src={playImg} className="player__bottom__bottom__play" onClick={handlePlaySong} />
+        <Image src={playingFlag ? playingImg :playImg} className="player__bottom__bottom__play" onClick={handlePlaySong} />
         <Image src={nextImg} className="player__bottom__bottom__nest-song" onClick={handleNextSong} />
         <Image src={ListImg} className="player__bottom__bottom__play-list" onClick={hanleSongList} />
       </View>
-      <View onClick={handleSeek}> 音频跳转测试</View>
+      {/* <View onClick={handleSeek}> 音频跳转测试</View> */}
     </View>
   );
 }
--- src/pages/Player/index.tsx
+++ src/pages/Player/index.tsx
@@ -1,7 +1,8 @@
 import { View} from "@tarojs/components";
 // import {useDispatch,useSelector }from 'react-redux'
 // import { SONG_LIST } from "@/src/stores/constants";
-import { get } from '@/services';
+import { useState } from 'react';
+// import { get } from '@/services';
 
 import './index.scss'
 
@@ -16,11 +17,15 @@ const demoSongList=[
   // {id: 1904183420, name:'请你一定要记得我好吗',url:'http://m701.music.126.net/20211221134640/c731000b50c4a9976b8da9d6f03ae98b/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/12140918564/0bc0/475a/cf27/4b3538f5999c1628659832c72462d1d4.mp3'},
   // {id: 1818831999, name:'道理的道理',url:'http://m701.music.126.net/20211221134653/67552f19e278286e68df7156a90781e7/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/12127818574/d7f4/0b2f/f38a/e1eb6ecfce778430613c27dd2434fdb9.mp3'},
   // {id: 1904055088, name:'At Christmas',url:'http://m801.music.126.net/20211221134705/eafc903d48d5cf63a5d9a79af4e5dfc5/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/12140383312/21b2/3445/4cbc/9fee02dd38c468981b04ba10eeea2cd3.mp3'},
-  {id:11111,name:'测试',url:'http://m8.music.126.net/20211221181029/d9097c7368ee61aa1f7f27952c0c4095/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/11955850260/5ce8/5cce/80ab/8cef3dc106566d2f938731ffea8110c3.mp3'}
+  // {id:11111,name:'测试',url:'http://m8.music.126.net/20211221181029/d9097c7368ee61aa1f7f27952c0c4095/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/11955850260/5ce8/5cce/80ab/8cef3dc106566d2f938731ffea8110c3.mp3'},
+  {id: 222, name:'测试',url:'http://localhost:8091/mp3/1897658456.mp3'},
+  {id:2233,name:'11',url:'http://localhost:8091/mp3/1903299149.mp3'}
 ]
 // const demoSongIDList = [1905096024,1904183420,1818831999,1904055088]
 
 export default function Palyer() {
+  const [isScroll, setIsScroll] = useState(false)// 是否播放
+  const [lyricsContent,setLyricsContent] = useState(0) // 记录当前播放位置
   // 模拟传递过来的数据
   // const dispatch = useDispatch();
   // dispatch({
@@ -34,82 +39,33 @@ export default function Palyer() {
   //   get(`/song/url?id=${demoSongIDList.join(',')}`).then((params) => {
   //   })
   // }
+  // 同步歌词事件
+  const handlSong  = (aduio,flag) => {
+    // console.log(flag,123)
+    setIsScroll(flag)
+    // aduio.onTimeUpdate((params) => {
+      // console.log(params,aduio.currentTime,12)
+    // })
+  }
+  const changeCurrentTime = (currentTime) =>{
+    setLyricsContent(currentTime)
+  }
   return (
     <View className="player">
       <Song />
-      {/* <View className="player__top">
-        <View className="player__top__top"> */}
-          {/* <View className="player__top__top__left"> */}
-          {/* 点击隐藏该页面，动画效果 */}
-            {/* <Image src={bottomImg}  className="player__top__top__left" /> */}
-          {/* </View> */}
-          {/* <View className="player__top__top__conter">
-            <View className="player__top__top__conter__main">
-              王娅-半吨兄弟-爱情错觉 */}
-              {/* 字幕滚动效果 */}
-            {/* </View>
-            <View className="player__top__top__conter__vice">
-              <View className="player__top__top__conter__vice__message">
-                筷子兄弟
-              </View>
-              <View className="player__top__top__conter__vice__add">
-                <Image src={addImg}  className="player__top__top__conter__vice__add__image" />
-              </View>
-            </View>
-          </View>
-          <View className="player__top__top__right">
-            <Image src={coverImg}  className="player__top__top__right__cover" /> */}
-            {/*  头像涟漪效果 */}
-            {/* <Image src={fengxiangImg}  className="player__top__top__right__share" />
-          </View>
-        </View> */}
-        {/* 音量添加减少效果 */}
-        {/* <View className="player__top__bottom">
-          <Image src={volumeImg}  className="player__top__bottom__volume" />
-          <View className="player__top__bottom__progress">
-            <Slider step={1} value={20} backgroundColor="rgba(255,255,255,0.2)" activeColor="rgba(255,255,255,0.5)" blockSize={15}   className="player__top__bottom__progress__slider" onChange={slideScroll} />
-          </View>
-          <Image src={readerImg}  className="player__top__bottom__radar" />
-
-        </View> */}
-      {/* </View> */}
-      {/* 播放页面头部开发 */}
-      {/* 歌词滚动效果、拖动歌词会跟着进行播放，拖动时会有线条和时间显示 */}
-      {/* <View className="player__conter">滚动歌词
-      </View> */}
-      <Lyric />
-
-      {/* 播放组件底部开发 --> 抽离成组件 */}
+      {/* 歌词显示/播放动画 */}
+      <View style={{height:'80%'}}>
+          <Lyric
+            isScroll={isScroll}
+            lyricsContent={lyricsContent}
+          />
+      </View>
+      {/* 播放组件底部开发 */}
       <Play
         demoSongList={demoSongList}
+        handlSong={handlSong}
+        changeCurrentTime={changeCurrentTime}
       />
-      {/* <View className="player__bottom">
-        <View className="player__bottom__top">
-          <View className="player__bottom__top__left">
-          <Image src={play1Img}  className="player__bottom__top__left__play" />
-          <Image src={starsImg}  className="player__bottom__top__left__stars" />
-          </View>
-          <View className="player__bottom__top__right">
-          <Image src={singImg}  className="player__bottom__top__right__sing" />
-          <Image src={moreImg}  className="player__bottom__top__right__more" />
-          </View>
-        </View>
-        <View className="player__bottom__conter"> */}
-          {/* 播放时间的移动 */}
-        {/* <View className="player__bottom__conter__start_time">00:00</View>
-        <View className="player__bottom__conter__progress"></View>
-        <View className="player__bottom__conter__end_time">02:34</View>
-        </View>
-        <View className="player__bottom__bottom"> */}
-          {/* <View className="player__bottom__bottom__play-order"></View> */}
-          {/* 顺序切换 */}
-          {/* <Image src={xunhuanImg}  className="player__bottom__bottom__play-order" />
-          <Image src={proImg}  className="player__bottom__bottom__last-song" />
-          <Image src={playImg}  className="player__bottom__bottom__play" />
-          <Image src={nextImg}  className="player__bottom__bottom__nest-song" />
-          <Image src={ListImg}  className="player__bottom__bottom__play-list" />
-        </View> */}
-      {/* </View> */}
     </View>
   );
 }
